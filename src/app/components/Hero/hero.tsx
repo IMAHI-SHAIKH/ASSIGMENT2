206e3f376d69db617c9c7197602721c458876b0c
IMAHI-SHAIKH <160288701+IMAHI-SHAIKH@users.noreply.github.com>
2024-10-27 23:43:11 +0500

    new files uploading error solving

---
+++ src/app/components/Hero/hero.tsx
@@ -0,0 +1,12 @@
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <h2>Hero Section</h2>
+      <p>Here, you can see my Hero Section</p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p> Here is my Next.js Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
