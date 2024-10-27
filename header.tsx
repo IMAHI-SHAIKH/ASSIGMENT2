56fb4d9e18d1edd40b6ee93fc87c4c4083c9dc66
IMAHI-SHAIKH <160288701+IMAHI-SHAIKH@users.noreply.github.com>
2024-10-27 22:55:56 +0500

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1> THIS IS MY ASSIGNMENT2</h1>
+      <h2> Here, you can see my Next.js Website</h2>
+    </header>
+  );
+};
+
+export default Header;
---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/header';
+
+import Footer from './components/footer/footer';
+
+import Hero from './components/Hero/hero';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header></Header>
+      <Hero></Hero>
+      <footer></footer>
+    </div>
+  );
+}
